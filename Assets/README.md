fbe4f84c7dd3d8bc630a14011b6a9c1958f26a29
D. Caine Winters <caine.winters@outlook.com>
2023-06-14 17:11:18 -0400

    Started README and added button to copy generated PW

---
+++ Assets/README.md
@@ -0,0 +1,8 @@
+# 03 Super Awesome Password Generator
+
+## By:  Caine Winters
+
+### How to use
+
+### Sources
+Aside from the provided starter code, a JS Math object based algorithm was used.  The code where this is used is so noted inline in the script.js file.
--- Develop/script.js
+++ Develop/script.js
@@ -12,71 +12,60 @@ function writePassword() {
 function generatePassword(){
   let noTypesSelected = true;
   let charSet = "";
-  while(noTypesSelected){
-    let passwordParams = {
-      "Number of digits": getNumberCharacters(),
-      "Lower Case": getTypes("Lower Case", "abcdefghijklmnopqurstuvwxyz"),
-      "Upper Case": getTypes("Upper Case", "ABCDEFGHIJKLMNOPQURSTUVWXYZ"),
-      "Numeric Characters": getTypes("Numeric Characters", "1234567890"),
-      "Special Characters": getTypes("Special Characters", "~!@#$%^&*()_+[]\\{}|;,./<>?")
-    }
-  
-    function getNumberCharacters(){
-      let digitsStr = prompt("How long would you like the password to be?\
-                            \nMust be at least 8 and no more than 128 characters.");
-      let digitsNum = -1;
-      while(true){
-        digitsNum = parseInt(digitsStr);
-        if(isNaN(digitsNum)){
-          console.log("Error, input must be a number");
-          digitsStr = prompt("ERROR!\nMust be a number.");
-        }
-        else if (digitsNum < 8 || digitsNum > 128) {
-          console.log("Error, input must be at least 8 and no more than 128 characters!");
-          digitsStr = prompt("ERROR!\nMust be at least 8 and no more than 128 characters.");
-        }
-        else{
-          return digitsNum;
-        }
-      }
-    }
-    
-  
-    function getTypes(thisType, typeSet){
-      let answer = prompt("Would you like "+ thisType +" included?\
-                          \nEnter \"/Y\" for yes or \"N\" for no.");
-      while(true){
-        if(answer == "y" || answer == "Y"){
-          noTypesSelected = false;
-          charSet = charSet + typeSet;
-          return true;
-        }
-        else if(answer == "n" || answer == "N"){
-          return false;
-        }
-        answer = prompt("Invalid input!\nWould you like "+ thisType +" included?\
-                          \nEnter \"Y\" for yes or \"N\" for no.");
-      }
-    }
-    /*
-    let passwordParams = [5];
-    // STOPPED RIGHT HERE!!  Trying do this with JSON??
-    passwordParams[0] = getNumberCharacters();
-    passwordParams[1] = getTypes("Lower Case");
-    passwordParams[2] = getTypes("Upper Case");
-    passwordParams[3] = getTypes("Numeric Characters");
-    passwordParams[4] = getTypes("Special Characters");
-    for(let i = 0; i < passwordParams.length; i++){
-      console.log(passwordParams[i]);
-    }
-    */
+
+  // PW generator loops until complete and correct input submitted
+  while(true){
+    let numDigits = getNumberCharacters();
+    getTypes("Lower Case", "abcdefghijklmnopqurstuvwxyz");
+    getTypes("Upper Case", "ABCDEFGHIJKLMNOPQURSTUVWXYZ");
+    getTypes("Numeric Characters", "1234567890");
+    getTypes("Special Characters", "~!@#$%^&*()_+[]\\{}|;,./<>?");
      
+   // If no types are selected return is bypassed for another loop
    if(noTypesSelected){
      console.log("Error, must select at least one input type!");
      alert("ERROR!\nMust select at least one input type.\nStarting over :(");
     }
     else{
-      return generate(passwordParams["Number of digits"]);
+      return generate(numDigits);
+    }
+  }
+  
+  // Private helper functions to encapsulate and/or abstract code
+  function getNumberCharacters(){
+    let digitsStr = prompt("How long would you like the password to be?\
+                          \nMust be at least 8 and no more than 128 characters.");
+    let digitsNum = -1;
+    while(true){
+      digitsNum = parseInt(digitsStr);
+      if(isNaN(digitsNum)){
+        console.log("Error, input must be a number");
+        digitsStr = prompt("ERROR!\nMust be a number.");
+      }
+      else if (digitsNum < 8 || digitsNum > 128) {
+        console.log("Error, input must be at least 8 and no more than 128 characters!");
+        digitsStr = prompt("ERROR!\nMust be at least 8 and no more than 128 characters.");
+      }
+      else{
+        return digitsNum;
+      }
+    }
+  }
+
+  function getTypes(thisType, typeSet){
+    let answer = prompt("Would you like "+ thisType +" included?\
+                        \nEnter \"/Y\" for yes or \"N\" for no.");
+    while(true){
+      if(answer == "y" || answer == "Y"){
+        noTypesSelected = false;
+        charSet = charSet + typeSet;
+        return true;
+      }
+      else if(answer == "n" || answer == "N"){
+        return false;
+      }
+      answer = prompt("Invalid input!\nWould you like "+ thisType +" included?\
+                        \nEnter \"Y\" for yes or \"N\" for no.");
     }
   }
 
@@ -89,6 +78,23 @@ function generatePassword(){
     }
     return result;
   }
+}
+
+// Added copy function to capture generated PW with click-button
+// Code taken from: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+function copyTextButton() {
+  // Get the text field
+  var copyText = document.getElementById("password");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 }
 
 // Add event listener to generate button
