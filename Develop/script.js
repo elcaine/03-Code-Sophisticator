c967af3a783e8429926ee6635daa63956e9ae43e
D. Caine Winters <caine.winters@outlook.com>
2023-06-13 23:20:20 -0400

    2 private functions for types and number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -6,59 +6,87 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
- 
+  passwordText.value = password; 
 }
+
 function generatePassword(){
-  let passwordParams = [5];
-  // STOPPED RIGHT HERE!!  Trying do this with JSON??
-  passwordParams[0] = ["Number Chars": getNumberCharacters()];
-  passwordParams[1] = ["Lowers", getTypes("Lower Case")];
-  passwordParams[2] = getTypes("Upper Case");
-  passwordParams[3] = getTypes("Numeric Characters");
-  passwordParams[4] = getTypes("Special Characters");
-  for(let i = 0; i < passwordParams.length; i++){
-    console.log(passwordParams[i]);
-  }
-  return passwordParams;
+  let noTypesSelected = true;
+  let strOut = "";
+  while(noTypesSelected){
+    let passwordParams = {
+      "Number of digits": getNumberCharacters(),
+      "Lower Case": getTypes("Lower Case"),
+      "Upper Case": getTypes("Upper Case"),
+      "Numeric Characters": getTypes("Numeric Characters"),
+      "Special Characters": getTypes("Special Characters")
+    }
+  
+    function getNumberCharacters(){
+      let digitsStr = prompt("How long would you like the password to be?\
+                            \nMust be at least 8 and no more than 128 characters.");
+      let digitsNum = -1;
+      while(true){
+        digitsNum = parseInt(digitsStr);
+        if(isNaN(digitsNum)){
+          console.log("Error, input must be a number");
+          digitsStr = prompt("ERROR!\nMust be a number.");
+        }
+        else if (digitsNum < 8 || digitsNum > 128) {
+          console.log("Error, input must be at least 8 and no more than 128 characters!");
+          digitsStr = prompt("ERROR!\nMust be at least 8 and no more than 128 characters.");
+        }
+        else{
+          return digitsNum;
+        }
+      }
+    }
+    
+  
+    function getTypes(thisType){
+      let answer = prompt("Would you like "+ thisType +" included?\
+                          \nEnter \"/Y\" for yes or \"N\" for no.");
+      while(true){
+        if(answer == "y" || answer == "Y"){
+          noTypesSelected = false;
+          return true;
+        }
+        else if(answer == "n" || answer == "N"){
+          return false;
+        }
+        answer = prompt("Invalid input!\nWould you like "+ thisType +" included?\
+                          \nEnter \"Y\" for yes or \"N\" for no.");
+      }
+    }
+    /*
+    let passwordParams = [5];
+    // STOPPED RIGHT HERE!!  Trying do this with JSON??
+    passwordParams[0] = getNumberCharacters();
+    passwordParams[1] = getTypes("Lower Case");
+    passwordParams[2] = getTypes("Upper Case");
+    passwordParams[3] = getTypes("Numeric Characters");
+    passwordParams[4] = getTypes("Special Characters");
+    for(let i = 0; i < passwordParams.length; i++){
+      console.log(passwordParams[i]);
+    }
+    */
+  
+    function buildSource(){
+
+    }
+   
+   if(noTypesSelected){
+     console.log("Error, must select at least one input type!");
+     alert("ERROR!\nMust select at least one input type.\nStarting over :(");
+    }
+    else{
+      let keys = Object.keys(passwordParams);
+      for(let i = 0; i < keys.length; i++){
+        strOut = strOut + passwordParams[keys[i]] + " / ";
+      }
+      return strOut;
+    }
+  }  
 }
 
-function getNumberCharacters(){
-  let digitsStr = prompt("How long would you like the password to be?\
-                        \nMust be at least 8 and no more than 128 characters.");
-  let inputNeeded = true;
-  let digitsNum = -1;
-  while(inputNeeded){
-    inputNeeded = false;
-    digitsNum = parseInt(digitsStr);
-    if(isNaN(digitsNum)){
-      console.log("Error, input must be a number");
-      inputNeeded = true;
-      digitsStr = prompt("ERROR!\nMust be a number.");
-    }
-    else if (digitsNum < 8 || digitsNum > 128) {
-      console.log("Error, input must be at least 8 and no more than 128 characters!");
-      inputNeeded = true;
-      digitsStr = prompt("ERROR!\nMust be at least 8 and no more than 128 characters.");
-    }
-  }
-  return digitsNum;
-}
-
-function getTypes(thisType){
-  let answer = prompt("Would you like "+ thisType +" included?\
-                      \nEnter \"/Y\" for yes or \"N\" for no.");
-  while(true){
-    if(answer == "y" || answer == "Y"){
-      return true;
-    }
-    else if(answer == "n" || answer == "N"){
-      return false;
-    }
-    answer = prompt("Invalid input!\nWould you like "+ thisType +" included?\
-                      \nEnter \"Y\" for yes or \"N\" for no.");
-  }
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
